bb23548d7861923cda8be2182c0ec8a9a4ae0e71
thayakawa-litalico <takato.hayakawa@litalico.co.jp>
2019-04-22 14:03:12 +0900

    add_api_rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,33 @@
 import * as React from "react";
 import { NextStatelessComponent } from "next";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 import Layout from "../components/Layout";
 
-interface PostLinkProps {
-  id: string;
-  title: string;
-}
-
-const PostLink: NextStatelessComponent<PostLinkProps> = ({ title, id }) => (
-  <li>
-    <Link as={`/p/${id}`} href={`/post?title=${title}`}>
-      <a>{title}</a>
-    </Link>
-  </li>
-);
-
-const Index: NextStatelessComponent<{}> = ({}) => (
+const Index = props => (
   <Layout>
-    <h1>Blog Posts</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
 
 export default Index;
